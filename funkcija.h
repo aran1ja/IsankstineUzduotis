3d8f03df05055584974a5b824aa6651260344d92
aran1ja <147089828+aran1ja@users.noreply.github.com>
2024-06-05 14:56:08 +0300

    Add files via upload

--- funkcija.h
+++ funkcija.h
@@ -10,16 +10,16 @@
 #include <regex>
 #include <algorithm>
 #include <iomanip>
+#include <unordered_set>
 
 using namespace std;
 
 bool failoAtidarymas(const string& failoPasirinkimas);
 string tvarkomeZodzius(const string& zodis);
 bool ieskomeURL(const string& zodis);
+void ieskotiZodziusSuAly(const string& zodis, map<string, vector<int>>& sarasas, int eilutesSkaicius);
 void zodziuIrasymasSarase(const string& zodis, map<string, vector<int>>& sarasas, int eilutesSkaicius);
-void failoSkaitymas(const string& failoPasirinkimas, map<string, vector<int>>& sarasas, vector<string>& url);
-void irasymasIFaila(const map<string, vector<int>>& sarasas, const vector<string>& url);
+void failoSkaitymas(const string& failoPasirinkimas, map<string, vector<int>>& sarasas, map<string, vector<int>>& ALYsarasas, vector<string>& url);
+void irasymasIFaila(const map<string, vector<int>>& sarasas, map<string, vector<int>>& ALYsarasas, const vector<string>& url);
 
 #endif
-
-
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "funkcija.h"
 
-//kazku klaida: what(): invalid special open parenthesis
-
+// Funkcija, tikrinanti ar pavyksta atidaryti faila
 bool failoAtidarymas(const string& failoPasirinkimas) {
     ifstream failas(failoPasirinkimas);
     return failas.good();
 }
 
+// Funkcija, tvarkanti zodzius (pasalina ne zodzio dalis ir pavercia maz raidemis)
 string tvarkomeZodzius(const string& zodis) {
     regex neZodziuDalis("[^\\w]");
     string tinkamasZodis = regex_replace(zodis, neZodziuDalis, " ");
@@ -14,15 +14,26 @@ string tvarkomeZodzius(const string& zodis) {
     return tinkamasZodis;
 }
 
+const unordered_set<string> neURLsarasas = {
+    "jpg", "jpeg", "doc", "docx", "ppt", "pptx", "pdf", "xls", "xlsx", "zip", "gif", "txt", "exe", "png", "accdb", "svg", "obj"
+};
+
+// Funkcija, ieskanti ar zodis yra URL
 bool ieskomeURL(const string& zodis) {
-    regex url("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?");
+    regex url ("(((http|https)://)?www\\.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)");
     return regex_match(zodis, url);
 }
 
-void zodziuIrasymasSarase(const string& zodis, map<string, vector<int>>& sarasas, int eilutesSkaicius) {
-    if (zodis.empty())
-        return;
+// Funkcija, ieskanti zodziu su ALY
+void ieskotiZodziusSuAly(const string& zodis, map<string, vector<int>>& sarasas, int eilutesSkaicius) {
+    size_t zodisSuAly = zodis.find("aly");
+    if (zodisSuAly < zodis.size()) {
+        zodziuIrasymasSarase(zodis, sarasas, eilutesSkaicius);
+    }
+}
 
+// Funkcija, pridedanti zodi i sarasa ir nuorodanti kurioje vietoje jis randasi
+void zodziuIrasymasSarase(const string& zodis, map<string, vector<int>>& sarasas, int eilutesSkaicius) {
     auto ieskome = sarasas.find(zodis);
 
     if (ieskome == sarasas.end()) {
@@ -37,10 +48,12 @@ void zodziuIrasymasSarase(const string& zodis, map<string, vector<int>>& sarasas
     }
 }
 
-void failoSkaitymas(const string& failoPasirinkimas, map<string, vector<int>>& sarasas, vector<string>& url) {
+// Zodziu nuskaitymas is failo
+void failoSkaitymas(const string& failoPasirinkimas, map<string, vector<int>>& sarasas, map<string, vector<int>>& ALYsarasas, vector<string>& url) {
     ifstream failas(failoPasirinkimas);
     string eilute;
     int eilutesSkaicius = 0;
+    
 
     while (getline(failas, eilute)) {
         eilutesSkaicius++;
@@ -53,15 +66,21 @@ void failoSkaitymas(const string& failoPasirinkimas, map<string, vector<int>>& s
             } else if (!arYraSkaicius) { 
                 string tinkamasZodis = tvarkomeZodzius(zodis);
                 istringstream irasomeTikTinkamusZodzius(tinkamasZodis);
+                size_t zodisSuAly = zodis.find("aly");
                 while (irasomeTikTinkamusZodzius >> zodis) {
+                    if (zodisSuAly < zodis.size()) {
+                    zodziuIrasymasSarase(zodis, ALYsarasas, eilutesSkaicius);
+                } else {
                     zodziuIrasymasSarase(zodis, sarasas, eilutesSkaicius);
+                }
                 }
             }
         }
     }
 }
 
-void irasymasIFaila(const map<string, vector<int>>& sarasas, const vector<string>& url) {
+// Rezultatai irasomi i naujas faila 'rezultatas.txt'
+void irasymasIFaila(const map<string, vector<int>>& sarasas, map<string, vector<int>>& ALYsarasas, const vector<string>& url) {
     ofstream rezultatas("rezultatas.txt");
     if (!rezultatas.good()) {
         cout << "Failo nepavyko sukurti." << endl;
@@ -72,6 +91,8 @@ void irasymasIFaila(const map<string, vector<int>>& sarasas, const vector<string
     for (const auto& urls : url) {
         rezultatas << urls << endl;
     }
+
+    rezultatas << " " << endl;
 
     if (!sarasas.empty()) {
         rezultatas << left << setw(20) << "Zodziai:" << setw(21) << " Zodziu kiekis:" <<  "Zodziai yra tokiose eilutese:" << endl;
@@ -90,10 +111,27 @@ void irasymasIFaila(const map<string, vector<int>>& sarasas, const vector<string
         }
     }
 
+    if (!ALYsarasas.empty()) {
+        rezultatas << left << setw(20) << "Zodziai su 'aly':" << endl;
+        rezultatas << "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+
+        for (const auto& i : ALYsarasas) {
+            if (i.second.size() > 1) {
+                rezultatas << left << setw(20) << i.first << setw(7) << "|" << setw(7) << i.second.size() << setw(7) << "|";
+
+                for (int j : i.second) {
+                    rezultatas << setw(4) << j;
+                }
+
+                rezultatas << endl;
+            }
+        }
+    }
+
     rezultatas.close();
 }
 
-int  main() {
+int main() {
 
     string failoPasirinkimas = "text.txt";
 
@@ -102,13 +140,15 @@ int  main() {
         return 0;
     }
 
+    map<string, vector<int>> ALYsarasoIsvedimas;
     map<string, vector<int>> sarasoIsvedimas;
     vector<string> url;
 
-    failoSkaitymas(failoPasirinkimas, sarasoIsvedimas, url);
-    irasymasIFaila(sarasoIsvedimas, url);
+    failoSkaitymas(failoPasirinkimas, sarasoIsvedimas, ALYsarasoIsvedimas, url);
+    irasymasIFaila(sarasoIsvedimas, ALYsarasoIsvedimas, url);
 
     cout << "Rezultatas gautas, ji perziureti galima sukurtame faile 'rezultatas.txt'." << endl;
 
     return 0;
 }
+
